79fa6055c3a5aa68d56d16317cae3e6eaefd77a5
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-12 14:19:48 +0530

    update

--- packer/scripts/install_ansible_ubuntu.sh
+++ packer/scripts/install_ansible_ubuntu.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -xe
 sudo apt-get -y update
-sudo apt-get -y install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get -y update
 sudo apt-get -y install ansible
-
-
